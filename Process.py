c6dbb1b05f626482aa8e805d466bb452635a9b16
little-nerd <a184999@gmail.com>
2015-03-10 22:44:10 +0800

    [new]Process

---
+++ Process.py
@@ -0,0 +1,23 @@
+man = []
+other = []
+try:
+	data = open('sketch.txt')
+
+	for each_line in data:
+		try:
+			(role, line_spoken) = each_line.split(':', 1)
+			"""strip() is clean space in string"""
+			line_spoken = line_spoken.strip()
+			if role == 'Man':
+				man.append(line_spoken)
+			elif role == 'Other man':
+				other.append(line_spoken)
+		except ValueError:
+			pass
+
+	data.close()
+except IOError:
+	print('The data file is missing!!!!')
+
+print(man)
+print(other)
