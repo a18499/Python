4b1bb64ab8657be479968c058dfed31f11349988
little-nerd <a184999@gmail.com>
2015-04-06 11:01:28 +0800

    [new]parse

--- Parse.py
+++ Parse.py
@@ -1,10 +1,20 @@
+def Browser(Parammeter):
+	if 'browser' in Parammeter:
+		print("found browser")
+	if 'contacts' in Parammeter:
+		print("found contact!")
+
+
+
+
 the_file = open('provider-log')
 print(the_file.readline(),end='\n')
-
+print(the_file.readline(),end='\n')
 print("Read Mutiple Line",end='\n')
 """ Back to the first line!!! """
 the_file.seek(0) 
 for each_line in the_file: 
-	print(each_line,end='\n')
+	Browser(each_line)
+	#print(each_line,end='\n')
 print("===========================",end='\n')
 the_file.close()
