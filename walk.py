27138a21635c95781332e452db740f7503a5d97d
peter <a184999@gmail.com>
2015-04-28 11:19:23 +0800

    [update]walk.py

--- walk.py
+++ walk.py
@@ -3,36 +3,72 @@ import subprocess
 
 
 file_list = []
-flag =0
+APKProtect_flag =0
+Bangcle_flag =0
+Ijiami_flag = 0
+
 count_ckeck =0
-#file_out = subprocess.check_output(['ls /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/virus/'],shell=True)
-file_out = subprocess.check_output(['ls /home/peter/test_virus/'],shell=True)
+file_out = subprocess.check_output(['ls /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Virus2/'],shell=True)
+#file_out = subprocess.check_output(['ls /home/peter/test_shell_sample/'],shell=True)
 file_list = str(file_out).strip("b '").strip("'").split("\\n")
 for each_word in file_list:
 	print(each_word,end ="\n")
-	os.system("unzip "+"/home/peter/test_virus/"+each_word+" -d /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/"+each_word+"/")
-	for root, dirs, files in os.walk("/media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/"+each_word):
-    		print (root)
-    		print (dirs)
-    		print (files)
-    		for each_file in files:
-    			if each_file == "libsecexe.x86.so":
-    				print("find it!!!!!!!!!!!!!!!!!!!!!!!!Bangcle",end ="\n")
-    				flag =1
-    			elif  each_file == "libsecexe.so":
-    				print("find it!!!!!!!!!!!!!!!!!!!!!!!!Bangcle",end ="\n")
-    				flag =1
-    			elif each_file == "libspkprotect2.so":
-    				print("find it!!!!!!!!!!!!!!!!!!!!!!!!ApkProtect",end ="\n")
-    				flag =1
-    			elif each_file ==  "libexecmain.so ":
-    				print("find it!!!!!!!!!!!!!!!!!!!!!!!!ApkProtect",end ="\n")
-    				flag =1
-	if flag == 1:
-    		flag =0
+	os.system("unzip "+"/media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Virus2/"+each_word+" -d /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Tmp/"+each_word+"/")
+	for root, dirs, files in os.walk("/media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Tmp/"+each_word):
+    		try:
+    			print (root)
+    			print (dirs)
+    			print (files)
+    			for each_file in files:
+    				print(each_file,end ="\n")
+    				if each_file == "libsecexe.x86.so":
+    					print("find it!!!!!!!!!!!!!!!!!!!!!!!!Bangcle",end ="\n")
+    					Bangcle_flag =Bangcle_flag+1
+    				elif each_file == "libsecmain.x86.so":
+    					Bangcle_flag =Bangcle_flag+1
+    				elif  each_file == "libsecexe.so":
+    					print("find it!!!!!!!!!!!!!!!!!!!!!!!!Bangcle",end ="\n")
+    					Bangcle_flag =Bangcle_flag+1
+    				elif each_file == "libsecmain.so":
+    					print("find it!!!!!!!!!!!!!!!!!!!!!!!!Bangcle",end ="\n")
+    					Bangcle_flag =Bangcle_flag+1
+    				elif each_file == "libspkprotect2.so":
+    					print("find it!!!!!!!!!!!!!!!!!!!!!!!!ApkProtect",end ="\n")
+    					APKProtect_flag =1
+    				elif each_file ==  "libexecmain.so ":
+    					print("find it!!!!!!!!!!!!!!!!!!!!!!!!",end ="\n")
+    					Ijiami_flag =Ijiami_flag+1
+    				elif each_word == "libexec.so":
+    					Ijiami_flag =Ijiami_flag+1
+    		except :
+    			print("walk failed")
+	print("Bangcle_flag!!!!!!= "+str(Bangcle_flag))
+	if APKProtect_flag == 1:
+    		APKProtect_flag =0
     		count_ckeck=count_ckeck +1
+    		print("in APKProtect_flag!!!!!!!!!!!!!!!!",end ="\n")
     		print("Check"+str(count_ckeck)+" file")
+    		os.system("cp -a -r /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Tmp/"+each_word+"/"+" /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Apkprotect/")
+    		os.system("rm -r -f  /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Tmp/"+each_word)
+	elif Bangcle_flag > 3:
+		Bangcle_flag = 0
+		count_ckeck=count_ckeck +1
+		print("in Bangcle_flag!!!!!!!!!!!!!!!!",end ="\n")
+		os.system("cp -a -r /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Tmp/"+each_word+"/"+" /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Bangcle/")
+		os.system("rm -r -f  /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Tmp/"+each_word)
+		print("Check"+str(count_ckeck)+" file")
+	elif Ijiami_flag > 1:
+		Ijiami_flag =0
+		count_ckeck=count_ckeck +1
+		print("in Ijiami_flag!!!!!!!!!!!!!!!!",end ="\n")
+		os.system("cp -a -r /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Tmp/"+each_word+"/"+" /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Ijiami/")
+		os.system("rm -r -f  /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Tmp/"+each_word)
+		print("Check"+str(count_ckeck)+" file")
 	else:
+		APKProtect_flag =0
+		Bangcle_flag = 0
+		Ijiami_flag = 0
 		count_ckeck=count_ckeck +1
 		print("Check"+str(count_ckeck)+" file")
-		os.system("rm -r -f  /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/"+each_word)
+		if each_word != "":
+			os.system("rm -r -f  /media/peter/3812e316-a581-434a-98a2-3ea81a27fd10/Tmp/"+each_word)
